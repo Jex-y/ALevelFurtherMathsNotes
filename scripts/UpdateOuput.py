344e40fa18d4e58d406870622bf7a4a062da2b18
Jex-y <edwardjex@live.co.uk>
2019-11-06 17:40:10 +0000

    Changed Layout of output

--- scripts/UpdateOuput.py
+++ scripts/UpdateOuput.py
@@ -4,13 +4,13 @@ import shutil
 def recursive_copy(start_path, target_base,relative=""):
 	for item in os.listdir(start_path + "\\" + relative):
 		if os.path.isdir(start_path + relative + "\\" + item):
-			if not os.path.isdir(target_base + relative + "\\" + item):
-				os.mkdir(target_base + relative + "\\" + item)
-				print("Created dir" + target_base + relative + "\\" + item)
 			recursive_copy(start_path, target_base,relative+"\\"+item)
-		elif item.split(".")[-1] == "pdf": #and not os.path.exists(target_base + relative + "\\" + item)
+		elif item.split(".")[-1] == "pdf" and item != "master.pdf": #and not os.path.exists(target_base + relative + "\\" + item)
 			print("Coppied " + item)
-			shutil.copyfile(start_path + relative + "\\" +item, target_base + relative + "\\" + item)
+			if not os.path.isdir("\\".join((target_base + relative).split("\\")[:-1])):
+				os.mkdir("\\".join((target_base + relative).split("\\")[:-1]))
+				print("Created dir" + "\\".join((target_base + relative).split("\\")[:-1]))
+			shutil.copyfile(start_path + relative + "\\" +item, target_base + relative + ".pdf")
 
 os.chdir("F:\\School\\L6\\Maths\\ALevelFurtherMathsNotes\\src")
 recursive_copy(".\\","..\\Output")
